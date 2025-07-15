791139dfb9fd5c0e0836db2c3879ff67255e7e8d
Neeraj Krishnan V M <neeraj.krishnan@appingtechnology.com>
2025-07-15 10:55:17 +0530

    added

--- resume_mailer/mailing/views.py
+++ resume_mailer/mailing/views.py
@@ -3,18 +3,28 @@ from django.core.mail import EmailMessage
 from .models import Mailing, MailingLog
 from .forms import MailingForm
 
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from .forms import MailingForm
+from .models import MailingLog
+
 def create_mailing(request):
     if request.method == 'POST':
         form = MailingForm(request.POST, request.FILES)
         if form.is_valid():
             mailing = form.save()
 
+            # Static subject and template
+            subject = "Resume Submission for Your Review"
+            html_content = render_to_string("mailing/emails/email_template.html")  # Fully static HTML
+
             recipients = mailing.mailing_list.contacts.all()
 
             for contact in recipients:
                 email = EmailMessage(
-                    subject=mailing.template.subject,
-                    body=mailing.template.html_content,
+                    subject=subject,
+                    body=html_content,
                     from_email=mailing.from_email,
                     to=[contact.email],
                 )
@@ -42,6 +52,7 @@ def create_mailing(request):
     else:
         form = MailingForm()
     return render(request, 'mailing/create_mailing.html', {'form': form})
+
 
 def mailing_success(request):
     return render(request, 'mailing/success.html')
